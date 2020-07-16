2285053d7f093845257c7216cf3929c91890f690
IrinaYu <irinayurchenko27@gmail.com>
2020-07-16 21:44:19 +0300

    Parametrs

--- src/test/java/TestWithParametrs.java
+++ src/test/java/TestWithParametrs.java
@@ -1,15 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import utils.WebDriverFactory;
 
 public class TestWithParametrs {
 
+  WebDriver driver = null;
+
   @Parameters({"waitTimeOutInSeconds"})
-  @Test
-  public static void testWithParametrsAnnotation(String waitTimeOutInSeconds) {
-    System.out.println("Timeout " + waitTimeOutInSeconds);
+
+  @BeforeMethod
+  public void setUp() {
+    WebDriverFactory.createInstance("Chrome");
+    driver = WebDriverFactory.getDriver();
   }
 
   @Test
   public static void testWithParametrsInListener() {
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    driver.quit();
   }
 }
