8f634dc03d398e675b826898caa077f2ee687e8d
IrinaYu <irinayurchenko27@gmail.com>
2020-08-17 12:46:29 +0300

    fix empty tests

--- src/test/java/EmptyTests.java
+++ src/test/java/EmptyTests.java
@@ -1,24 +1,28 @@
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utils.WebDriverFactory;
 
 public class EmptyTests {
+  WebDriver driver = null;
 
   @BeforeMethod
   public void beforeMethod() {
+    WebDriverFactory.createInstance("Chrome");
+    driver = WebDriverFactory.getDriver();
     System.out.println("BeforeMethod");
   }
 
   @Test
   public void test1() {
-    assert 1 == 1;
+    assert 1==1;
     System.out.println("test1");
   }
 
   @Test
   public void test2() {
-    assert 1 == 1;
+    assert 1!=0;
     System.out.println("test2");
   }
 
